d89d9c26e796cf433aac884a53f2df264f3a516e
Dominik Mrówka <dmrowka7@gmail.com>
2022-04-25 13:21:53 +0200

    Add function Page switch, get data from API, add function getCorrectAuthorName

---
+++ src/js/script.js
@@ -0,0 +1,101 @@
+const templates = {
+  player: Handlebars.compile(document.querySelector('#template-player').innerHTML)
+};
+
+// Pages switch
+
+const allPageBtn = document.querySelectorAll('.page-btn');
+
+for (let pageBtn of allPageBtn) {
+  pageBtn.addEventListener('click', function () {
+    console.log('this', this);
+    if (!this.classList.value.includes('active')) {
+
+      for (let pageBtn of allPageBtn) {
+        pageBtn.classList.remove('active');
+        const allPages = document.querySelectorAll('.page');
+        for (let page of allPages) {
+          page.classList.remove('active-page');
+        }
+      }
+      this.classList.add('active');
+      document.querySelector('.' + this.id).classList.add('active-page');
+    }
+  });
+}
+
+// API
+
+const url = '//localhost:3131' + '/' + 'songs';
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (parsedResponse) {
+    // console.log('parsed', parsedResponse);
+
+    //GET DATA FROM API
+
+    for (let oneSong of parsedResponse) {
+      // console.log('one', oneSong);
+
+      const songId = oneSong.id;
+      const songTitle = oneSong.title;
+      // const songAuthor = oneSong.author;
+      const songFileName = oneSong.filename.toLowerCase();
+      // const songCategories = oneSong.categories;
+
+      // console.log(songAuthor, songCategories);
+
+
+      // GET DATA FOR FORMAT AUTHOR NAME
+
+      const nameSongAndAuthor = songFileName.split('_').join(' ').split('.mp3').join('').split('-').join('');
+      const songTitleLowerCase = songTitle.toLowerCase();
+      const authorName = nameSongAndAuthor.split(songTitleLowerCase).join('');
+
+      // console.log('authorName', authorName);
+      // console.log(songTitle.toLowerCase());
+
+
+      // PUSH DATA TO dataContent
+      dataContent(songId, authorName, songTitle);
+
+      //PUSH DATA TO authorAndTitle
+      getCorrectAuthorName(authorName);
+    }
+  });
+
+
+const dataContent = function (id, author, title) {
+
+  const generatedData = { id: id, author: author, title: title };
+  // console.log('generatedDara', generatedData);
+  const pushGeneratedData = templates.player(generatedData);
+  // console.log('push', pushGeneratedData);
+  // let html = '';
+  console.log(pushGeneratedData);
+  // html += pushGeneratedData;
+};
+
+
+const getCorrectAuthorName = function (author) {
+  // console.log('authorName', author);
+
+  const authorTrim = author.trim();
+
+  formatName(authorTrim);
+  console.log(formatName(authorTrim));
+
+  function formatName(name) {
+
+    const firstNameAndLastName = name.split(' ');
+    let firstName = firstNameAndLastName[0];
+    let lastName = firstNameAndLastName[1];
+    firstName = firstName.charAt(0).toUpperCase() + firstName.substr(1).toLowerCase();
+    lastName = lastName.charAt(0).toUpperCase() + lastName.substr(1).toLowerCase();
+    return firstName + ' ' + lastName;
+  }
+
+};
